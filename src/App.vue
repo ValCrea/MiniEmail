5be931359b7a15dd9e7b2246935cb0effa5c32b0
ValCrea <val.waldner@creatim.com>
2022-09-22 13:27:30 +0200

    init vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <section>
+      <input type="text" />
+    </section>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.app {
+  background-color: $white;
+}
+</style>
